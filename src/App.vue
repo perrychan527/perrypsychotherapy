066077671738305c88014c3908b42561b52493bf
perrychan527 <41201506+perrychan527@users.noreply.github.com>
2021-07-29 02:40:11 +0100

    deleted tabs, added padding and margin

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,12 @@
       color="teal lighten-4"
       flat
     >
-      <v-toolbar-title>Perry W.T. Chan, psychotherapist & counsellor</v-toolbar-title>
+      <v-toolbar-title class="flex text-center">Perry W.T. Chan, psychotherapist & counsellor</v-toolbar-title>
 
     </v-toolbar>
     <v-tabs 
     centered
     center-active>
-      <v-tab>
-        welcome
-      </v-tab>
       <v-tab>
         bio
       </v-tab>
@@ -23,98 +20,88 @@
       <v-tab>
         practicalities
       </v-tab>
-      <v-tab>
-        contact
-      </v-tab>
-      <v-tab>
-        resources
-      </v-tab>
       <v-tab-item>
         <v-card flat>
           <v-card-text>
-            <p>
-              Two tiny eggs in the bird's nest
-            </p>
-            
-            <p>
-            Dream the undreamable
-            </p>
-            
-            <p>
-            Speak the unspeakable
-            </p>
-            
-            <p>
-            Feel the "unfeelable"
-            </p>
-            
-            <p>
-            Formulate the unformulated
-            </p>
-            
-            <p>
-            Experience the understandable
-            </p>
-            
-            <p>
-            (A poem written by myself at the beginning of my training. It basically sums up what is possibly going on in our relational space)
-            </p>
+            <v-container class="px-16 mx-16"> 
+              <p>
+               Hello. You can call me Perry. I am a (COSCA) qualified psychotherapist and counsellor based in Edinburgh, Scotland. I am doing my private practice and, at the same time, working in different counselling centres. We can talk in either English, Cantonese or Mandarin. 
+              </p>
 
-            <p class="mb-0">
-            </p>
-            <v-img  src="@/assets/perrybird.jpeg"
-                    max-width="500"
-                    max-height="300"
-            ></v-img>
+              <p>
+               Get in touch:
+              </p>
+              perrywtchan@gmail.com
+              <p>
+
+              </p>
+
+              <p>
+
+              </p>
+
+              <p class="mb-0">
+              </p>
+              
+              <v-img  src="@/assets/perrybio.jpg"
+                      max-width="200"
+                      max-height="400"
+              ></v-img>
+            </v-container> 
           </v-card-text>
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
           <v-card-text>
-            <p>
-              You can call me Perry. I am a (COSCA) qualified psychotherapist and counsellor based in Edinburgh, Scotland. I am doing my private practice and, at the same time, working in different counselling centres. We can talk in either English, Cantonese or Mandarin. 
-            </p>
+            <v-container class="px-16">
+              <p>
+                My approach is psychodynamic and person-centred. In fact, I don’t like to categorise. So more accurately speaking, my approach is relational. It is two people sitting together trying to discover who we really are, what is perpepuating our dysfunctional thoughts or behaviours, and what is hindering us from self-actualisation and feeling fulfilled. Psychotherapy, in my opinion, is not about fixing something. It’s all about making sense of painful experience, developing self-awareness. As a result, we can have higher capacity to embrace more not-knowing in life and to make better decisions without repeating our unhealthy patterns.
+              </p>
 
-            <p>
+              <p class="mb-0">
+                I don't like labels such as xxx disorder, xxx syndrome as I find it pathologising and over-powering. I see each person as an individual with unique stories, experiences, personalities, behavioural patterns, complexes, psyches, souls......which are all meaningful, whether or not they are making sense for you just yet at this moment. We together try to make sense of something painful and unbearable, create meanings out of it, and thus experience psychic transformation. Hmmm.....it might sound vague. To give you an analogy, imagine both of us trying to map a dark foggy city together with a compass and find the way out. Though it might be scary, it is a transformative journey. In our work, this is a safe space for us to let this exquisite alchemical reaction happen. It is simple......but deep…...very deep......
+              </p>
+              
+              <v-img  src="@/assets/perryfish.jpg"
+                      max-width="800"
+                      max-height="400"
+              ></v-img>
+              <p>
+                Two tiny eggs in the bird's nest
+              </p>
+              
+              <p>
+              Dream the undreamable
+              </p>
+              
+              <p>
+              Speak the unspeakable
+              </p>
+              
+              <p>
+              Feel the "unfeelable"
+              </p>
+              
+              <p>
+              Formulate the unformulated
+              </p>
+              
+              <p>
+              Experience the understandable
+              </p>
+              
+              <p>
+              (A poem written by myself at the beginning of my training. It basically sums up what is possibly going on in our relational space)
+              </p>
 
-            </p>
-
-            <p>
-
-            </p>
-
-            <p>
-
-            </p>
-
-            <p class="mb-0">
-            </p>
-            
-            <v-img  src="@/assets/perrybio.jpg"
-                    max-width="200"
-                    max-height="400"
-            ></v-img>
-            
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <v-card-text>
-            <p>
-              My approach is psychodynamic and person-centred. In fact, I don’t like to categorise. So more accurately speaking, my approach is relational. It is two people sitting together trying to discover who we really are, what is perpepuating our dysfunctional thoughts or behaviours, and what is hindering us from self-actualisation and feeling fulfilled. Psychotherapy, in my opinion, is not about fixing something. It’s all about making sense of painful experience, developing self-awareness. As a result, we can have higher capacity to embrace more not-knowing in life and to make better decisions without repeating our unhealthy patterns.
-            </p>
-
-            <p class="mb-0">
-              I don't like labels such as xxx disorder, xxx syndrome as I find it pathologising and over-powering. I see each person as an individual with unique stories, experiences, personalities, behavioural patterns, complexes, psyches, souls......which are all meaningful, whether or not they are making sense for you just yet at this moment. We together try to make sense of something painful and unbearable, create meanings out of it, and thus experience psychic transformation. Hmmm.....it might sound vague. To give you an analogy, imagine both of us trying to map a dark foggy city together with a compass and find the way out. Though it might be scary, it is a transformative journey. In our work, this is a safe space for us to let this exquisite alchemical reaction happen. It is simple......but deep…...very deep......
-            </p>
-            
-            <v-img  src="@/assets/perryfish.jpg"
-                    max-width="800"
-                    max-height="400"
-            ></v-img>
-            
+              <p class="mb-0">
+              </p>
+              <v-img  src="@/assets/perrybird.jpeg"
+                      max-width="500"
+                      max-height="300"
+              ></v-img>
+            </v-container class="px-16">
           </v-card-text>
         </v-card>
       </v-tab-item>
