b65f6c85f53996ce0c04ebd40b3dfb5800f3f46e
jerry7767 <death-J@hotmail.com>
2021-08-18 02:01:55 +0900

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,8 @@ git commit -m 'deploy'
 
 # 將 dist資料夾中的內容推送至遠端分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 #git push -f https://github.com/perrychan527/perrypsychotherapy.git master:deploy
-git push -f git@github.com:perrychan527/perrychan527.github.io.git master
+#git push -f git@github.com:perrychan527/perrychan527.github.io.git master
+git push -f https://github.com/perrychan527/perrychan527.github.io.git master
 cd -
 rm dist/ -rf
 
