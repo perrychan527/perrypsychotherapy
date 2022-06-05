69223c3b36cfeb2fb92a956d6c9567826da2fccc
perrychan527 <41201506+perrychan527@users.noreply.github.com>
2022-06-05 02:11:09 +0100

    updated chinese

--- src/locale/tc.js
+++ src/locale/tc.js
@@ -4,7 +4,7 @@ export const locale = {
   TAB1: "關於",
   TAB2: "治療",
   TAB3: "約見需知",
-  TAB4: "我是誰",
+  TAB4: "Memories",
   WELCOMETITLE: "歡迎",
   PROPIC: "當我還在馬來西亞工作的時候",
   WELCOME: "Hi, you can call me Perry. I'm based in Edinburgh, Scotland. I provide counselling & psychotherapy in English, Cantonese or Mandarin mainly for clients based in the UK and HK. I'm also a counselling tutor and a researcher (writing up my doctoral thesis at this moment) at the University of Edinburgh."
